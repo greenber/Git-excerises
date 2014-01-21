e9ecc3cf23f4ba81cdfc7e6eeed9dc7ba8a9ea84
greenber <jeeberdee@yahoo.com>
2014-01-21 10:56:58 -0600

    Update Hello.java
    Changed the , into !.

--- lab1/src/Hello.java
+++ lab1/src/Hello.java
@@ -1,9 +1,8 @@
-
 public class Hello
 {
   public static void 
   main (String[] args) 
 {
-  System.out.println ("Hello, GitHub!");
+  System.out.println ("Hello! GitHub!");
 } // main(String[])
 }
